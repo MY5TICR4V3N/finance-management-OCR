fe0fe3ffd6d1f7dd77429715c7964e36fe9a8851
binoy-ce <binoychullikkal@protonmail.com>
2024-02-29 23:39:49 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text,View,SafeAreaView} from 'react-native'
+
+const FinManager= () => {
+    return(
+        <View style={{flex:1}}>
+            
+        </View>
+    )
+}
+
+
+export default FinManager
