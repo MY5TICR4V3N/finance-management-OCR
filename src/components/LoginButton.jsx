36d7c3ad01155c57f2d850d6cbb04e6dfa7782c4
binoy-ce <binoychullikkal@protonmail.com>
2024-03-25 22:25:14 +0530

    now login works

---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Button} from 'react-native-paper';
+import auth from '@react-native-firebase/auth';
+import {useState} from 'react';
+import {Snackbar} from 'react-native-paper';
+import {Portal} from 'react-native-paper';
+import {View, Text} from 'react-native';
+
+const LoginButton = ({buttonName, navigation, email, password}) => {
+	
+	const [isVisible, setIsVisible] = useState(false);
+    const onDismissSnackBar = () => setIsVisible(false);
+	const onToggleSnackbar = () => setIsVisible(!isVisible);
+
+	const CheckLogin = async () => {
+		try {
+			await auth().signInWithEmailAndPassword(
+				email,
+				password,
+			);
+			navigation.navigate('users');
+		} catch (error) {
+			// console.log(error);
+			onToggleSnackbar();
+		}
+	};
+
+	return (
+		<View>
+			<Button
+				mode="outlined"
+				buttonColor="#24A19C"
+				textColor="white"
+				height={60}
+				labelStyle={{fontSize: 18}}
+				style={{justifyContent: 'center'}}
+				onPress={CheckLogin}>
+				{buttonName}
+			</Button>
+            <Portal>
+  <Snackbar
+    visible={isVisible}
+    onDismiss={onDismissSnackBar}
+ >
+    Invalid Email/Password
+  </Snackbar>
+</Portal>
+		</View>
+	);
+};
+
+export default LoginButton;
--- src/screens/LoginPage.jsx
+++ src/screens/LoginPage.jsx
@@ -1,125 +1,140 @@
-
-import React from 'react'
-import { 
-    StyleSheet,
-    Text,
-    View,
-    SafeAreaView,
-    
-    Image
-} from 'react-native'
-import WhiteStatus from '../components/WhiteStatus'
-import { TextInput,Button } from 'react-native-paper'
-import { useState } from 'react'
-
-import BigButton from '../components/BigButton'
+import React from 'react';
+import {StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import WhiteStatus from '../components/WhiteStatus';
+import {TextInput, Button} from 'react-native-paper';
+import {useState} from 'react';
 
 
+import LoginButton from '../components/LoginButton';
 
-const LoginPage= ({navigation}) => {
-    const [isVisible,setIsVisible] = useState(false)
+const LoginPage = ({navigation}) => {
+	const [isVisible, setIsVisible] = useState(false);
+    //authentication stuff
+    const [email,SetEmail] = useState('')
+    const [password,SetPassword] = useState()
+    // const [loading,setLoading] = useState(false)
 
-    const setVisiblity =()=> {
-        setIsVisible(!isVisible);
-    }
-    return(
-        <SafeAreaView style={{backgroundColor:"white"}}>
-            <WhiteStatus/>
-            <Image
-                source={require('../assets/images/Carrot.png')}
-                style={styles.logo}
-            />
-            <View style={styles.loginWrapper}>
-                <Text style={styles.login}>
-                    LOGIN
-                </Text>
-                <Text style={styles.loginDesc}>
-                    Enter your username and password
-                </Text>
-                <Text style={styles.NamePass}>
-                    Username
-                </Text>
-                <TextInput/>
-                <Text style={[styles.NamePass,{marginTop:9}]}>
-                    Password
-                </Text>
-                <TextInput
-                secureTextEntry={!isVisible}
-                right={<TextInput.Icon icon={isVisible ? "eye-slash" : "eye"} onPress={setVisiblity} />}
+
+
+
+
+	const setVisiblity = () => {
+		setIsVisible(!isVisible);
+	};
+	return (
+		<SafeAreaView style={{backgroundColor: 'white'}}>
+			<WhiteStatus />
+			<Image
+				source={require('../assets/images/Carrot.png')}
+				style={styles.logo}
+			/>
+			<View style={styles.loginWrapper}>
+				<Text style={styles.login}>LOGIN</Text>
+				<Text style={styles.loginDesc}>
+					Enter your Email and Password
+				</Text>
+				<Text style={styles.NamePass}>Email</Text>
+				<TextInput
+                value={email}
+                onChangeText={text => SetEmail(text)}
                 />
-                <Text style={styles.forget}>
-                    Forgot Password?
-                </Text>
-
-            </View>
-            <View style={styles.buttonWrapper}>
-            <BigButton name={"login"} topage={"users"} navigation={navigation}/>
-
-            </View>
-            <Text style={styles.lastText}>
-                Dont have an account?  
-                <Text 
-                    onPress={()=>navigation.navigate('signup')}
-                style={{color:"#53B175"}}> Signup</Text>
-            </Text>
-
-        </SafeAreaView>
-
-    )
-}
+				<Text style={[styles.NamePass, {marginTop: 9}]}>
+					Password
+				</Text>
+				<TextInput
+					secureTextEntry={!isVisible}
+                    value = {password}
+                    onChangeText={text => SetPassword(text)}
+					right={
+						<TextInput.Icon
+							icon={
+								isVisible
+									? 'eye-slash'
+									: 'eye'
+							}
+							onPress={setVisiblity}
+						/>
+					}
+				/>
+				<Text style={styles.forget}>
+					Forgot Password?
+				</Text>
+			</View>
+			<View style={styles.buttonWrapper}>
+				<LoginButton
+					buttonName={'Login'}
+					email={email}
+                    password={password}
+					navigation={navigation}
+                    
+				/>
+			</View>
+			<Text style={styles.lastText}>
+				Dont have an account?
+				<Text
+					onPress={() =>
+						navigation.navigate('signup')
+					}
+					style={{color: '#53B175'}}>
+					{' '}
+					Signup
+				</Text>
+			</Text>
+		</SafeAreaView>
+	);
+};
 
 const styles = StyleSheet.create({
-    logo:{
-        marginTop:"10%",
-         height:80,
-         width:80,
-        alignSelf:"center",
-        resizeMode:"contain"
-        
-    },
-    loginWrapper:{
-        marginLeft:21,
-        // backgroundColor:"red",
-        marginTop:66,
+	logo: {
+		marginTop: '10%',
+		height: 80,
+		width: 80,
+		alignSelf: 'center',
+		resizeMode: 'contain',
+	},
+	loginWrapper: {
+		marginLeft: 21,
+		// backgroundColor:"red",
+		marginTop: 66,
+	},
+	login: {
+		fontSize: 35,
+		fontFamily: 'OpenSansSemiBold',
+		color: 'black',
+		marginBottom: 5,
+	},
+	loginDesc: {
+		fontFamily: 'OpenSans',
+		fontSize: 19,
+		color: '#7C7C7C',
+		marginBottom: 18,
+	},
+	NamePass: {
+		fontFamily: 'OpenSansBold',
+		fontSize: 18,
+		color: '#7C7C7C',
+	},
+	forget: {
+		fontStyle: 'italic',
+		alignSelf: 'flex-end',
+		fontFamily: 'OpenSansLight',
+		marginTop: 18,
+		fontSize: 17,
+		marginRight: 10,
+	},
+	buttonWrapper: {
+		marginTop: 47,
+		marginLeft: 10,
+		marginRight: 10,
+	},
 
-    },
-    login:{
-        fontSize:35,
-        fontFamily:"OpenSansSemiBold",
-        color:"black",
-        marginBottom:5    },
-    loginDesc:{
-        fontFamily:"OpenSans",
-        fontSize:19,
-        color:"#7C7C7C",
-        marginBottom:18
-    },
-    NamePass:{
-        fontFamily:"OpenSansBold",
-        fontSize:18,
-        color:"#7C7C7C"
-    },
-    forget:{
-        fontStyle:"italic",
-        alignSelf:"flex-end",
-        fontFamily:"OpenSansLight",
-        marginTop:18,
-        fontSize:17,
-        marginRight:10
-    },
-    buttonWrapper:{
-        marginTop:47,
-        marginLeft:10,
-        marginRight:10
-    },
+	lastText: {
+		marginTop: 15,
+		alignSelf: 'center',
+		fontFamily: 'OpenSansBold',
+		color: '#181725',
+		fontSize: 17,
+	},
+});
 
-    lastText:{
-        marginTop:15,
-        alignSelf:"center",
-        fontFamily:"OpenSansBold",
-        color:"#181725",
-        fontSize:17
-    }
-   });
-
-export default LoginPage
+export default LoginPage;
